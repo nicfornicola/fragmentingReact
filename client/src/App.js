1c5c000d23941f2f7cbcfd6b2c808957cdf9f343
ndf17a <ndf17a@acu.edu>
2023-12-02 15:12:49 -0600

    lots of changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,62 +2,204 @@
 
 import React from "react";
 import "./App.css";
+import NumberPicker from "react-widgets/NumberPicker";
+
 
 function App() {
     const [pathList, setPathList] = React.useState([]);
+    const [percent, setPercent] = React.useState(0);
+    const [file, setFile] = React.useState(null);
+    const [loops, setLoops] = React.useState(10);
+    const [fragButtonClass, setFragButtonClass] = React.useState("fragButtonInVis");
+    const [imgName, setImgName] = React.useState("");
 
     React.useEffect(() => {
-        fetch("/list")
-        .then((res) => res.json())
-        .then((data) => setPathList(JSON.parse(data.list)));
+        getList();
+        getImgName();
     }, []);
 
-    console.log(pathList)
+    React.useEffect(() => {
+        fetch("/loops")
+        .then((res) => res.json())
+        .then((data) => {
+            setPercent(JSON.parse(1/data.loops))
+            setLoops(JSON.parse(data.loops))
+        });
 
-    const listItems = pathList.map((path) =>
-        <li key={path}>
-            <img className="img" src={path} alt="fraggedImage"/>
-        </li>
-    );
+    }, []);
+
+    const getList = (event) => {
+        fetch("/list")
+        .then((res) => res.json())
+        .then((data) => {
+            setPathList(JSON.parse(data.list))
+        });
+    }; 
+
+    const getImgName = (event) => {
+        fetch("/imageName")
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data.imgName)
+            if(data.imgName !== "") {
+                setFragButtonClass("fragButtonVis");
+                setImgName(data.imgName.charAt(0).toUpperCase() + data.imgName.slice(1))
+            } 
+        });
+    }; 
+
+    const original = pathList.slice(0,1).map((path) => {
+        
+        let title = "Original " + imgName;
+        return ( 
+            <div key={path}>
+                <div>{title}</div>
+                <img className="img" src={path} alt="fraggedImage"/>
+            </div>
+        )
+    });
+
+    let currentPercent = percent;
+    const listItems = pathList.slice(1).map((path) => {
+        
+        let title = Math.round(currentPercent * 100)  + "%";
+        currentPercent += percent;
+
+        return ( 
+            <li key={path}>
+                <div>{title}</div>
+                <img className="img" src={path} alt="fraggedImage"/>
+            </li>
+        )
+        
+    });
+
+    const handleFileChange = (event) => {
+        setFile(event.target.files[0]);
+    };
+    
+    const handleUploadAndSetPath = async (event) => {
+        event.preventDefault(); 
+        if (file) {
+            await upload(file, setFragButtonClass);
+        }
+    };
+
+    // adding more images means I want to decrease the rate at which the image disapears
+    const handleLoopChange = (newLoops) => {
+        setLoops(newLoops);
+        callBackEndLoops(newLoops)
+    };
+
+    const handleFrag = () => {
+        window.location.reload();
+
+        fetch("/frag")
+          .then((res) => res.json())
+          .then((data) => console.log(data.message))
+          .catch((error) => console.error("Error calling /frag endpoint:", error));
+    };
+
+    const handleReset = () => {
+        window.location.reload();
+
+        callBackEndLoops(10);
+        setLoops(10);    
+        setImgName("");
+        callBackEndResetName();
+        setFragButtonClass("fragButtonInVis");
+    };
 
     return (
     <div className="App">
       <header className="App-header">
         <div className="input-group">
-            <label htmlFor='files'>Select files </label>
-            <input
-                type="file"
-                name="myImage"
-                onChange={(event) => {
-                    console.log(event.target.files[0]);
-                    upload(event.target.files[0]);
-                }}
-            />            
+            <label htmlFor='file'>Select Image to Fragment </label>
+            <form className="form" onSubmit={handleUploadAndSetPath}>
+                <input
+                    type="file"
+                    name="myImage"
+                    onChange={handleFileChange}
+                />
+                <button
+                    type="submit"
+                    className="submit-btn"
+                >
+                Upload
+                </button>
+            </form>
+            <button
+                type="button"
+                className={fragButtonClass}
+                onClick={handleFrag}
+            >
+                Fragment {imgName}
+            </button>
+            <br/>
+            <label>Number of Images</label>
+            <NumberPicker
+                value={loops}
+                onChange={value => handleLoopChange(value)}
+            />
+
+            <button 
+                type="button"
+                className="resetButton"
+                onClick={handleReset} >
+            Reset Values
+            </button>
+        
         </div>
-        <form action="/frag">
-            <button id="fragButton">Fragment</button>
-        </form>
+        
+        
+
+        <div className="help">
+            <ol>
+                <li>Choose File</li>
+                <li>Upload</li>
+                <li>Fragment</li>
+            </ol>
+        </div>
+        
         <br/>
-        Original: <br/>
-        <ul> 
+        {original}
+        <ol> 
             {listItems}
-        </ul>
+        </ol>
       </header>
     </div>
   );
 }
 
-function upload(file) {
+async function callBackEndLoops(newLoops) {
+    const formData = new FormData();
+    formData.append("newLoops", newLoops);
+  
+    await fetch("/new_loops", {
+      method: "POST",
+      body: formData,
+    });
+}
+
+async function callBackEndResetName() {
+    const formData = new FormData();
+    formData.append("newName", "");
+  
+    await fetch("/reset_name", {
+      method: "POST",
+      body: formData,
+    });
+}
+
+async function upload(file, setFragButtonClass) {
     const formData = new FormData();
     formData.append("file", file);
-    
-    fetch("/upload_file", {
-        method: 'POST',
-        body: formData
-    
+    // const response = 
+    await fetch("/upload_file", {
+      method: "POST",
+      body: formData,
     })
-    .then((res) => console.log(res))
-    .catch((err) => ("Error occured", err));    
+
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -14,67 +14,149 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname+'/public'));
 
 
+
 // Image name to be fragmented
 let imgName = "";
 // How many images will be created and fragmented
 let loops = 10
-let imageDir = '/public/images/';
-let imageDirPath = imageDir + 'fragged/' + imgName;
 
 app.get('/list', async (req, res) => {
-        let imgPathFragged ="./client" + imageDirPath + '/' + imgName + 'Fragged' + loops + '.png'
-        setTimeout(function() { // Wait for the files to be written before trying to read them
-            fs.readFile(imgPathFragged, (err, data) => {
-                // Check if a fragged file exists
-                if (!err && data) {
-                    // Setup template for html tags
-                    
-                    // Start at one so we ignore the 0, just show original
-                    let arrayList = [];
-                    let pathName = imgName + "/" + imgName;
-                    arrayList.push("/images/uploads/" + imgName + ".png")
-
-                    for(let i = 1; i <= loops; i++) {
-                        // Send to front to make '<img class="img" src="/images/fragged/{{imgName}}/{{imgName}}Fragged{{fragNumA}}.png" alt="Fragment{{fragNumA}}">' +
-                        arrayList.push("/images/fragged/" + pathName + "Fragged" + i + ".png")
+        if(imgName != "") {
+            let imgPathFragged = "./client/public/images/fragged/" + imgName + "/" + imgName + 'Fragged' + 1 + '.png'
+            setTimeout(function() { // Wait for the files to be written before trying to read them
+                fs.readFile(imgPathFragged, (err, data) => {
+                    // Check if a fragged file exists
+                    if (!err && data) {
+                        
+                        // Start at one so we ignore the 0, just show original
+                        let arrayList = [];
+                        let pathName = imgName + "/" + imgName;
+                        arrayList.push("/images/uploads/" + imgName + ".png")
+    
+                        for(let i = 1; i <= loops; i++) {
+                            arrayList.push("/images/fragged/" + pathName + "Fragged" + i + ".png")
+                        }
+                        res.json({ 
+                            list: JSON.stringify(arrayList), 
+                            loops: loops
+                        });
+                    } else {
+                        console.log("No fragged images at " + imgPathFragged)
                     }
-                    res.json({ list: JSON.stringify(arrayList) });
-                } else {
-                    console.log("No fragged at " + imgPathFragged)
-                }
-                
-            })
-
-        }, 50);
-
+                    
+                })
+    
+            }, 100);
+        } else {
+            console.log("ERROR: imgName is empty")
+        }
+        
 });
 
-// This is called when Fragment button is clicked
-app.get('/frag', async (req, res) => {
-    fragmentImage();
-    res.send('Fragment!');
+app.get('/loops', async (req, res) => {
+    res.json({ 
+        loops: loops
+    });
+});
+
+app.get('/imageName', async (req, res) => {
+    res.json({ 
+        imgName: imgName
+    });
+});
+
+app.post('/new_loops', upload.none(), async (req, res) => {
+    loops = req.body.newLoops;
+    res.status(200).json({ message: 'Data received successfully', data: loops });
+});
+
+app.post('/reset_name', upload.none(), async (req, res) => {
+    imgName = req.body.newName;
+    res.status(200).json({ message: 'Data received successfully', data: imgName });
 });
 
 app.post("/upload_file", upload.single("file"), uploadFile);
 async function uploadFile(req, res) {
-    console.log("UPLOAD")
     try {
         // Assuming you are uploading an image in PNG format
         const processedImage = await sharp(req.file.buffer).toFormat('png').toBuffer();
-
+        
         // Modify the file name as needed
-        const imgName = req.file.originalname.toLowerCase();
-        const uploadedPath = "./client" + imageDir + 'uploads/' + imgName;
+        imgName = req.file.originalname.toLowerCase().replace(".png","");
+        const uploadedPath = "./client/public/images/uploads/" + req.file.originalname.toLowerCase();
 
         fs.writeFile(uploadedPath, processedImage, (err) => {
             if (err) throw err;
-            console.log(`Image ${imgName} saved.`);
+            console.log(`Image ${uploadedPath} saved.`);
         });
 
-        res.json({ message: 'Successfully uploaded and processed file' });
+        res.status(200).json({ message: 'Successfully uploaded and processed file' });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
+// This is called when Fragment button is clicked
+app.get('/frag', async (req, res) => {
+    if(imgName != "") {
+        fragmentImage();
+        res.send({ message: 'Fragment!' });
+    } else {
+        console.log("Error: imgName is empty")
+    }
+        
+});
+const fragmentImage = async () => {
+    // let killMap = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+    let killMap = []
+    let pixelSize = 20;
+
+    for(let i; i < pixelSize; i++) {
+        killMap.push([]);
+    }
+
+    // Create the folder path
+    if (fs.existsSync("./client/public/images/fragged/" + imgName)) {
+        console.log("Deleting " + "./client/public/images/fragged/" + imgName)
+        await fs.promises.rm("./client/public/images/fragged/" + imgName, { recursive: true })
+    }
+
+    await fs.promises.mkdir("./client/public/images/fragged/" + imgName, { recursive: true })
+    console.log("\nNew Fragmented pictures: " + "./client/public/images/fragged/" + imgName)
+
+    // Original image path 
+    let uploadImgPath = "./client/public/images/uploads/" + imgName + ".png";
+    console.log("Original image used: " + uploadImgPath + "\n")
+    
+
+    // Loop through n times to create n new images
+    let counter = 1;
+
+    for (let factorOfFragment = 1; factorOfFragment <= loops; factorOfFragment++) {
+       
+        // Create new PNG and start the process
+        let readStream = fs.createReadStream(uploadImgPath);
+        readStream.pipe(new PNG()).on('parsed', function() {
+
+            
+            // Fill kill map with current factorOfFragment percentage of true so we know which chunks to delete
+            fillMap(factorOfFragment, killMap, pixelSize);
+            // Delete the n x n chunks, this = the new png
+            deleteChunks(this, killMap, pixelSize);
+
+            // Set the path for the new image to be created
+            // i.e. "./public/images/fragged/frog/frogFragged6.png"
+            let imgPathFragged = "./client/public/images/fragged/" + imgName + "/" + imgName + 'Fragged' + counter + '.png'
+            console.log("Fragmenting " + factorOfFragment + "0%...")
+            console.log("Created: " + imgPathFragged)
+
+            // Write the new image to the file
+            let writeStream = fs.createWriteStream(imgPathFragged);
+            this.pack().pipe(writeStream);
+            readStream.close();
+            counter++;
+        })
     }
 }
 
@@ -84,24 +166,25 @@ app.listen(port, () => {
 
 
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+    return Math.ceil(Math.random() * max);
 }
 
-function fillMap(factorOfFragment, killMap) {
+function fillMap(factorOfFragment, killMap, pixelSize) {
     // Fill the killMap with trues where we are going to kill stuff
-    for (let y = 0; y < 17; y++) {
-        for (let x = 0; x < 17; x++) {
-            let rint = getRandomInt(50)
+    for (let y = 0; y < pixelSize+1; y++) {
+        for (let x = 0; x < pixelSize+1; x++) {
+            let rint = getRandomInt(loops)
             // If chunk is false and random number is less then factor, kill this chunk = true
-            if(!killMap[y][x] && rint < factorOfFragment) {
+            if(!killMap[y][x] && rint <= factorOfFragment) {
                 killMap[y][x] = true;
             } 
         }
     }
+
 }
 
-function deleteChunks(img, killMap) {
-    // Keeps tracks of 16x16 chunks 
+function deleteChunks(img, killMap, pixelSize) {
+    // Keeps tracks of n x n chunks 
     let yCounter = 0;
     let xCounter = 0;
     for (let y = 0; y < img.height ; y++) {
@@ -118,15 +201,15 @@ function deleteChunks(img, killMap) {
                 pixels[i + 2] = 255;
             }
 
-            // If we are at a multiple of 16 then this ends the chunk, move on to next
-            if(x % Math.ceil(img.width/16) == 0) {
+            // If we are at a multiple of pixelSize then this ends the chunk, move on to next
+            if(x % Math.ceil(img.width/pixelSize) == 0) {
                 xCounter++;
             }
         }
         // Reset x chunk counter since we are going down a row
         xCounter = 0;
-        // If we are at a multiple of 16 then this ends the chunk, move on to next
-        if(y % Math.ceil(img.height/16) == 0)
+        // If we are at a multiple of pixelSize then this ends the chunk, move on to next
+        if(y % Math.ceil(img.height/pixelSize) == 0)
             yCounter++;
     }
 
@@ -134,39 +217,3 @@ function deleteChunks(img, killMap) {
     return img;
 }
 
-const fragmentImage = async () => {
-    let killMap = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
-
-    // Create the folder path
-    await fs.promises.mkdir(imageDirPath, { recursive: true })
-    console.log("\nNew Fragmented pictures: " + imageDirPath)
-
-    // Original image path 
-    let uploadImgPath = imageDir + 'uploads/'+ imgName;
-    console.log("Original image used: " + uploadImgPath + "\n")
-    
-    // Set where the stream will read from
-    let readStream = fs.createReadStream(uploadImgPath);
-
-    // Loop through n times to create n new images
-    for (let factorOfFragment = 1; factorOfFragment <= loops; factorOfFragment++) {
-        // Create new PNG and start the process
-        readStream.pipe(new PNG()).on('parsed', function() {
-
-            // Fill kill map with current factorOfFragment percentage of true so we know which chunks to delete
-            fillMap(factorOfFragment, killMap);
-            // Delete the 16x16 chunks
-            deleteChunks(this, killMap);
-
-            // Set the path for the new image to be created
-            // i.e. "./public/images/fragged/frog/frogFragged6.png"
-            let imgPathFragged = imageDirPath + "/" + imgName + 'Fragged' + factorOfFragment + '.png'
-            console.log("Fragmenting " + factorOfFragment + "0%...")
-            console.log("Created: " + imgPathFragged)
-
-            // Write the new image to the file
-            let writeStream = fs.createWriteStream(imgPathFragged);
-            this.pack().pipe(writeStream);
-        })
-    }
-}
